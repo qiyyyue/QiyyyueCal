ca1c1ea70106953ec527a067296cbe765af09ea6
qiyyyue <qiyyyue@gmail.com>
2019-01-23 02:53:24 +0800

    add funcs

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-import flask
+import os, sys
+base_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(base_dir)
 from flask import Flask
 from Funcs.Login.login import login_bp
 from Funcs.Calculator.Calculator import cal_bp
 from Funcs.Reward.Reward import reward_bp
-
 
 app = Flask(__name__)
 
---
+++ init_path.py
@@ -0,0 +1,6 @@
+import os, sys
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(base_dir, 'Funcs/Calculator'))
+sys.path.append(os.path.join(base_dir, 'Funcs/Login'))
+sys.path.append(os.path.join(base_dir, 'Funcs/Reward'))
